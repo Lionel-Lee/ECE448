82516d88f6ed96822d1c6ebfcdc5eaf0f28cf9a0
mw26 <mw26@illinois.edu>
2020-11-14 18:22:13 +0800

    maybe finished

--- mp3/mp3-code-new/part3/perceptron.py
+++ mp3/mp3-code-new/part3/perceptron.py
@@ -12,11 +12,32 @@ def perceptron(X, y):
 
     '''
     P, N = X.shape
+    X=np.mat(X)
     w = np.zeros((P + 1, 1))
+    # print(w)
     iters = 0
     # YOUR CODE HERE
     # begin answer
     # TODO
+#print(X)
+    while True:
+        iters += 1
+        flag=0
+        for i in range(N):
+            x=X[:,i].transpose()
+            x=np.c_[x, np.array([1])]
+            result=np.dot(x,w)
+            result=np.sign(result)
+            # print(result)
+            if result != y[:,i]:
+                w += np.multiply(((1/(iters))) * np.array(y[:, i]) , x.transpose())
+                flag=1
+        if flag==0:
+            break      
     # end answer
-    
+    # print(w)
+    temp = w[2]
+    w = np.delete(w,2)
+    w = np.r_[temp, w]
+    # print(w)
     return w, iters
